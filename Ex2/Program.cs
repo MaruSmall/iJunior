ecd007049c21f376de210e617f2c37286b50c010
MaruSmall <marina_gritsanik@mail.ru>
2022-04-11 18:13:35 +0300

    Добавьте файлы проекта.

---
+++ Ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = -12;
+            uint speed = 10;
+            bool isDead=false;
+            string name = "Петя";
+            char ch = 'C';
+            float f = 15.09f;
+            double hp = 45.8;
+            var a=n1 + speed;
+            byte byte1 = 200;
+            int count = 0;
+        }
+    }
+}
