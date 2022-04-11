4d34b7644309555944cc088f19325f738bf8577d
MaruSmall <marina_gritsanik@mail.ru>
2022-04-11 18:48:03 +0300

    задачи 3, 4, 5

---
+++ ex4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ваше имя: ");
+            string name= Console.ReadLine();
+            Console.WriteLine("Ваш знак зодиака: ");
+            string znak= Console.ReadLine();
+            Console.WriteLine ($"Вас зовут "+ name +", вы по знаку зодиака - " + znak+" и вы проходите курс Романа Сакутина!");
+        }
+    }
+}
---
+++ ex5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 30;
+            Console.WriteLine("Переменные до обмена местами n1=" + n1 + ", n2=" + n2);
+            int help = 0;
+            help = n1;
+            n1 = n2;
+            n2 = help;
+            Console.WriteLine("Переменные после обмена местами n1=" + n1 + ", n2=" + n2);
+        }
+    }
+}
